23c9d6f54b06564b157dc1f5079ce7734079d1b4
kokodzh <kokodzh@DESKTOP-1JELUOD>
2016-07-06 20:58:05 +0300

    Done.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 class Array {
